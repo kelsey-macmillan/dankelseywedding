97adaae0627d376ae7d02b670aae36c9681e76ad
Kelsey MacMillan <Kelsey@Kelseys-MacBook.local>
2015-12-22 23:22:00 -0800

    made the whole damn website

--- main.py
+++ main.py
@@ -1,23 +1,80 @@
 import os
 import psycopg2
-from flask import Flask, request
+import json
+from flask import Flask, request, abort
 
 app = Flask(__name__, static_folder='static')
 
+# Serves Static HTML
 @app.route('/')
 def main():
     return app.send_static_file('index.html')
 
+# Find Guest and Return Guest Status
 @app.route('/rsvp-get/<name>')
 def rsvp_get(name):
+    
     conn = psycopg2.connect("dbname=Kelsey user=Kelsey")
     cur = conn.cursor()
-    cur.execute('SELECT * FROM guest;')
-    data=cur.fetchone()
+
+    guest = get_guest(cur,name)
+
     cur.close()
     conn.close()
-    return str(data)
 
+    return json.dumps(guest, ensure_ascii=False)
+
+# Updates Guest List with RSVP Info
+@app.route('/rsvp-set/<name>', methods=['POST'])
+def rsvp_set(name):
+    # TODO non existant input may cause an error
+    rsvp1 = request.form.get('rsvp1')
+    rsvp2 = request.form.get('rsvp2')
+    plus1_name = request.form.get('plus1_name')
+    note = request.form.get('note')
+
+    conn = psycopg2.connect("dbname=Kelsey user=Kelsey")
+    cur = conn.cursor()
+    
+    guest = get_guest(cur,name)
+
+    # TO DO ADD NOTE
+    if guest['name2'] == None and guest['plus1_allowed']:
+        cur.execute('UPDATE guest SET rsvp1=(%s), plus1_name=(%s) WHERE name1=(%s)',(rsvp1,plus1_name,name,))
+    elif guest['name2'] == None and not guest['plus1_allowed']:
+        cur.execute('UPDATE guest SET rsvp1=(%s) WHERE name1=(%s)',(rsvp1,name,))  
+    else:
+        cur.execute('UPDATE guest SET rsvp1=(%s), rsvp2=(%s) WHERE name1=(%s) OR name2=(%s)', (rsvp1,rsvp2,name,name,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    return ""
+
+# Create guest object with guest properties
+def create_guest(guest_cols):
+    guest={}
+    guest['name1']=guest_cols[0]
+    guest['name2']=guest_cols[1]
+    guest['plus1_allowed']=guest_cols[2]
+    guest['plus1_name']=guest_cols[3]
+    guest['rsvp1']=guest_cols[4]
+    guest['rsvp2']=guest_cols[5]
+    guest['note']=guest_cols[6]
+    return guest
+
+def get_guest(cursor,name):
+    cursor.execute('SELECT name1, name2, plus1_allowed, plus1_name, rsvp1, rsvp2, note ' +
+        'FROM guest WHERE name1 = (%s) OR name2 = (%s)', (name,name,))
+    guest_cols=cursor.fetchone()
+    # if guest is not on the list
+    if guest_cols == None:
+        abort(401)
+    # otherwise create the guest
+    guest=create_guest(guest_cols)
+    return guest
+
+# Debug logger
 if __name__ == "__main__":
     app.debug = True
     app.run()
--- static/js/rsvp.js
+++ static/js/rsvp.js
@@ -1,9 +1,76 @@
 $(document).ready(function() {
-    $('#rsvpForm').submit(function (event){
-        var name = $('#nameInput').val()
-        $.get('/rsvp-get/' + name, function(data){
-            alert(data)
-        })
+    // Guest submits name in RSVP field
+    $('#rsvp-form').submit(function (event){
         event.preventDefault()
+
+        // Hide the not on guest list error in case it has been shown
+        $('#not-on-guest-list').hide()
+
+        // Get name of guest from form and query backend
+        var name = $('#name-input').val()
+        $.ajax({
+            method: 'GET',
+            url: '/rsvp-get/' + name,
+        // Once query is successfully returned with guest info    
+        }).done(function(guest){
+            var guest=JSON.parse(guest);
+
+            // Determine correct form ID based on guest info
+            // if the guest does not have couple, and is allowed plus one
+            if (guest.name2 == null && guest.plus1_allowed) {
+                formID = 'one-guest-plus-one'
+            }
+            // if the guest does not have couple, and is not allowed plus one
+            else if (guest.name2 == null && !guest.plus1_allowed) {
+                formID = 'one-guest'
+            }
+            // otherwise the guest must be a couple
+            else {
+                formID = 'couple-guest'
+                $('#couple-guest .guest1-name').prepend(guest.name1)
+                $('#couple-guest .guest2-name').prepend(guest.name2)
+            }
+
+            // Update form shown
+            $('#rsvp-form .button-primary').hide()
+            $('#name-input').prop('readonly', true)
+            // TODO update form defaults
+            $('#' + formID + ' .plus-one-input').val(guest.plus1_name)
+            $('#' + formID).show()    
+        }).fail(function(){
+            // Give error if guest not on list
+            $('#not-on-guest-list').show()
+        })
     })
+                  
+    // Post guest RSVP
+    var submitCallback = function (event){
+        event.preventDefault()
+        var formID = event.currentTarget.id
+        $('#rsvp-submit-error').hide()
+        var name = $('#name-input').val()
+        var rsvp1 = $('#' + formID + ' .rsvp1-input').val()
+        var rsvp2 = $('#' + formID + ' .rsvp2-input').val()
+        var plus1_name = $('#' + formID + ' .plus-one-input').val()
+        var note = $('#' + formID + ' .note-input').val()
+        $.ajax({
+            method: 'POST',
+            url: '/rsvp-set/' + name,
+            data: {
+                'rsvp1': rsvp1,
+                'rsvp2': rsvp2,
+                'plus1_name': plus1_name,
+                'note': note
+            }
+        }).done(function(){
+            $(".rsvp-button").prop('value', 'RSVP Sent');
+        }).fail(function(){
+            $('#rsvp-submit-error').show()
+        })
+    }
+
+    // Call post RSVP function for each form
+    $('#one-guest-plus-one').submit(submitCallback)
+    $('#one-guest').submit(submitCallback)
+    $('#couple-guest').submit(submitCallback)
 })
